6f90a010effa43886dc796216ce3a00746053532
Savan Fami <savanfami1239@gmail.com>
2024-04-28 17:56:20 +0530

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-
-
-import './App.css'
+import { useState, useContext } from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import AuthProvider, { AuthContext } from "./Context/AuthContext";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Signup from "./Pages/Signup";
+import Login from "./Pages/Login";
+import AddProduct from "./Pages/AddProduct";
+import Footer from "./Components/Footer/Footer";
+import SubFooter from "./Components/Footer/SubFooter";
 
 function App() {
-
-
+  const user = useContext(AuthContext);
   return (
-    <>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-
+    <> 
+      <AuthProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/add-product" element={<AddProduct />} />
+        </Routes>
+        <Footer/>
+        <SubFooter/>
+      </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
